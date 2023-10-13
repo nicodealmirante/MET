764dc61a8b3af9619bc2c0cae07092b7baa1c615
nicodealmirante <nicolas.e.luna@gmail.com>
2023-10-13 04:31:28 -0300

    listo

--- app.js
+++ app.js
@@ -249,7 +249,7 @@ await flowDynamic(`*VALOR ESPEJO MAGICO* \n
 
 const main = async () => {
     const adapterDB = new JsonFileAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
+    const adapterFlow = createFlow([flowPrincipal, flowVenta, flowsAlquiler, Cliente, Menuflow, audiono])
 
     const adapterProvider = createProvider(MetaProvider, {
         jwtToken: 'EAAMziR3dWTwBO0dBceZArmJhZBQSEIygUsmBsKFw0LAyzdpLfznHunGZCbh4uvaHbZBp2ago7mZBds122x7KH3CoWFYG2JjGCltdUKroz8ZBMDxwbEGlFPMCdoVAwXK53UzYq68tTY0spXZBRbIazEsDgfe0wMZBAQggYogDS7F5GNTK26vo6toXy67HyJ2aZBu5WzFkyZCng58PnLj2o5ZBtOZAoK15uhG2rCZB9p6XZA',
