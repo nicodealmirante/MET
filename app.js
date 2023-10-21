4bd711370190e0c0be7c75226255727969987fb9
nicodealmirante <nicolas.e.luna@gmail.com>
2023-10-21 03:11:12 +0000

    modified:   app.js

--- app.js
+++ app.js
@@ -279,9 +279,9 @@ await flowDynamic(`*VALOR ESPEJO MAGICO* \n
   
     BotWrapper.initialize(BotCreate, {
       CHATWOOT_URL: "https://chatwoot-production-36d7.up.railway.app/",
-      CHATWOOT_ID:"3",
-      CHATWOOT_INBOX_ID:  "wshJN4RodjHhKosYNaVC7SMn",
-      CHATWOOT_API_ACCESS_TOKEN: "S6xhUEkzKByL8XE9NXuTYNEr",
+      CHATWOOT_ID:"1",
+      CHATWOOT_INBOX_ID:  "1",
+      CHATWOOT_API_ACCESS_TOKEN: "wshJN4RodjHhKosYNaVC7SMn",
     });
   };
   
