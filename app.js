b46b3b553fc09fc3d8f770dc05a6f5bf48b1bc73
nicodealmirante <nicolas.e.luna@gmail.com>
2023-10-22 06:54:40 -0300

    modified:   app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { readFileSync } = require("fs");
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 const fs = require("fs")
 const axios = require("axios");
-const ServerAPI = require('./http');
+const BotWrapper = require("./Services/class/botWrapper");
 
 let motivo;  
 ////////////////////////////////////////////////////////////////////////////////////////////
@@ -38,35 +38,49 @@ console.log('Numero Agendado de Alquiler');*/
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const Cliente = addKeyword(["AGEN-TE"],{sensitive:true})
-.addAnswer('Selfie Mirror 360 + Selfie', {
-    media: 'dibu.jpg'})
-    .addAnswer('Showroom', {media: 'video.mp4'})
+.addAnswer('Selfie Mirror 360 + Selfie',{media: 'dibu.jpg'})
+    .addAnswer('Showroom', {media: 'video.mp4', delay: 4000})
     .addAnswer('Selfie Mirror', {media: 'video2.mp4'})
     .addAnswer('Captura 360', {media: 'video360.mp4'})
-        .addAnswer("Tiene alguna consulta? En que horario podria llamarlo?", {capture:true, delay:5000}, async (ctx ,{endFlow,provider,gotoFlow}) => { 
-       
-         console_log("Tiene alguna consulta? En que horario podria llamarlo?")
-
+    .addAnswer("*ESTE CHAT AUTOMATICO FINALIZO.*", { 
+      capture: true,
+      buttons: [
+          {body: 'CONTINUAR CON AGENTE'},
+          {body: 'VOLVER AL MENU'},
+          {body: 'FINALIZAR'},
+      ],
+  }, async (ctx, { endFlow, gotoFlow, provider, flowDynamic}) => {
           const mywhatsa = "5491140054474@s.whatsapp.net";
-console_log(ctx.body)
-                  if(ctx.body == "SM" || ctx.body == "Sm" || ctx.body == "sm"){
-                    return gotoFlow(Menuflow),
-                    endFlow() }
-             await provider.sendtext(mywhatsa, `*${motivo}* \nNumero: +${ctx.from}\nNombre: *${ctx.pushName}*\nINFO: \n*${ctx.body}*`)})
+
+if (ctx.body == 'CONTINUAR CON AGENTE') {
+
+await provider.sendtext(mywhatsa, `*${motivo}* \nNumero: +${ctx.from}\nNombre: *${ctx.pushName}*\nINFO: \n*${ctx.body}*`)
+  flowDynamic('UN AGENTE SE COMUNICARA CON USTED A LA BREVEDAD')
+await endFlow()
+
+} else if (ctx.body == 'VOLVER AL MENU') {
+
+ await gotoFlow(Menuflow)
+
+  } else if (ctx.body == 'FINALIZAR') {
+    flowDynamic('GRACIAS POR COMUNICARSE CON NOSOTROS. QUEDAMOS A SUS ORDENES.')
+await endFlow()
+}}
+)        
+/** 
+})
          
-    .addAnswer("Buenisimo, A la brevedad me estare comunicando con usted. Tambien puede enviarme a mi numero personal 11-4005-4474. Muchisimas Gracias. Quedo a sus ordenes", {capture:true, delay:5000}, async (ctx ,{gotoFlow,endFlow,provider,fallBack}) => {
+    .addAnswer("ESTE CHAT AUTOMATICO A FINALIZADO. NOS PONDREMOS EN CONTACTO CON USTED", {capture:true, delay:5000}, async (ctx ,{gotoFlow,endFlow,provider,fallBack}) => {
           if(ctx.body == "SM" ||ctx.body == "Sm" || ctx.body == "sm"){
                return gotoFlow(Menuflow),
               endFlow()}
     const mywhatsa = "5491140054474@s.whatsapp.net";
-    console_log(ctx.body)
-
     await provider.sendtext(mywhatsa, `SIG MSJ\nNumero: +${ctx.from}\nINFO: *${ctx.body}*`) 
      return fallBack("Gracias por comunicarse con nosotros. Escriba *SM* para volver al menu inicial")
   }) 
+*/
 
-
-///////////////////////////////////////////////////////////////// EVENTO VOICE
+//////////////////////////////zx</////////////////////////////////// EVENTO VOICE
 
 const audiono = addKeyword(EVENTS.VOICE_NOTE)
   .addAnswer('Disculpe, no puedo escuchar audios. Por favor utilice solo texto.')
@@ -121,18 +135,52 @@ const flowsAlquiler = addKeyword(['//alqu-iler//'], {sensitive: true})
 
       .addAnswer(['🔒Los valores se congelan y la fecha se reserva solo al señar el servicio (2023)', 
              '🚚El valor no incluye traslados',
-             '🚩*Servicio disponible para todo el país.* Contamos con representantes en todas las provincias'],{capture:false}, async (ctx, {endFlow,gotoFlow }) => {
+             '🚩*Servicio disponible para todo el país.* Contamos con representantes en todas las provincias'],{capture:false}, async (ctx, {gotoFlow,endFlow,flowDynamic}) => {
        ///      numero2(ctx.from)
            motivo= "Alquiler";
-             await gotoFlow(Cliente);
-            endFlow()}
-                   )
+      console.log('GOTOCLIENTE');
+ 
+      await flowDynamic('Selfie Mirror 360 + Selfie',{media: 'dibu.jpg'})
+      await flowDynamic('Showroom', {media: 'video.mp4', delay: 4000});
+      await flowDynamic('Selfie Mirror', {media: 'video2.mp4'});
+      await flowDynamic('Captura 360', {media: 'video360.mp4'});
+      await flowDynamic("*ESTE CHAT AUTOMATICO FINALIZO.*", { 
+            capture: true,
+            buttons: [
+                {body: 'CONTINUAR CON AGENTE'},
+                {body: 'VOLVER AL MENU'},
+                {body: 'FINALIZAR'},
+            ],
+        }, async (ctx, { endFlow, gotoFlow, provider, flowDynamic}) => {
+                const mywhatsa = "5491140054474@s.whatsapp.net";
+      
+      if (ctx.body == 'CONTINUAR CON AGENTE') {
+       await flowDynamic('En que fecha y donde se realizara el evento?') ,{capture: true},async (ctx, { endFlow, gotoFlow, provider, flowDynamic}) => {
+            const mywhatsa = "5491140054474@s.whatsapp.net";
+
+      await provider.sendtext(mywhatsa, `*${motivo}* \nNumero: +${ctx.from}\nNombre: *${ctx.pushName}*\nINFO: \n*${ctx.body}*`)
+        flowDynamic('UN AGENTE SE COMUNICARA CON USTED A LA BREVEDAD' );
+      await endFlow()}
+      } else if (ctx.body == 'VOLVER AL MENU') {
+      
+       await gotoFlow(Menuflow)
+      
+        } else if (ctx.body == 'FINALIZAR') {
+          await flowDynamic('GRACIAS POR COMUNICARSE CON NOSOTROS. QUEDAMOS A SUS ORDENES.')
+      await endFlow()
+      }}
+      )        
+            }
+      )
+
+
+
 
 /////////////////////////////////////////////////////////////////////////////////////////// FLUJO VENTA
 
 
   const flowVenta = addKeyword(['VE-NTA'], { sensitive: true })
-          .addAnswer('👌 Te envío la info de Venta.')
+          .addAnswer('👌 Te envío la info de Venta.',{delay: 2000})
 
           .addAnswer(['*Espejo Mágico Selfie Mirror*',
                     '\nEl Espejo Mágico de Selfie Mirror cuenta con una cámara web de alta calidad, vidrio templado resistente, una Mini PC y un',
@@ -143,6 +191,76 @@ const flowsAlquiler = addKeyword(['//alqu-iler//'], {sensitive: true})
                     'y fiestas.'  ])
 
         .addAnswer('Plataforma 360 Super Slow', {media: 'banner.jpg'})
+    .addAnswer(
+        'OK',
+        {
+            capture: true
+        },
+        async (ctx, {provider}) => {
+            const headerText = 'Selfie Mirror'
+            const bodyText = '*Seleccione una opcion*'
+            const footerText = 'Sistema Automatico FAQ'
+            const buttonList = 'OPCIONES'
+            const listParams = [
+                {
+                    title: '*VENTA*',
+                    rows: [
+                        {
+                            id: 'ID_1',
+                            title: 'ESPEJO MAGICO SELFIE MIRROR',
+                            description: 'VENTA'
+                        },
+                        {
+                            id: 'ID_2',
+                            title: 'PLATAFORMA 360',
+                            description: 'SUPER SLOW'
+                        },
+                        {
+                            id: 'ID_3',
+                            title: 'ORDENADORES DE FILA LED',
+                            description: 'FILA VIP'
+                        }
+                    ]
+                },
+                {
+                    title: 'ALAQUILER',
+                    rows: [
+                      {
+                        id: 'ID_1',
+                        title: 'ESPEJO MAGICO ',
+                        description: 'SELFIE MIRROR'
+                    },
+                    {
+                        id: 'ID_2',
+                        title: 'PLATAFORMA 360',
+                        description: 'SUPER SLOW'
+                    },
+                    ]
+                  },
+                {
+                  title: 'LA EMPRESA',
+                  rows: [
+                      {
+                          id: 'ID_1',
+                          title: 'UBICACION',
+                          description: 'SHOWRROOM'
+                      },
+                      {
+                          id: 'ID_2',
+                          title: 'PAGINA WEB',
+                          description: 'PORTALES DIGITALES'
+                      },
+                      {
+                          id: 'ID_3',
+                          title: 'HABLAR CON ASESOR',
+                          description: 'WHATSAPP ASESOR'
+                      }
+                  ]
+                },
+            ]
+            await provider.sendList(ctx.from, headerText, bodyText, footerText, buttonList ,listParams)
+        }
+    )
 
         .addAnswer(['La facilidad de uso es una de las ventajas clave del Espejo Mágico. Simplemente tienes que enchufarlo y presionar el ',
                       'botón de encendido para que empiece a funcionar. Esto agiliza la instalación y permite que los eventos comiencen rápidamente.',
@@ -171,35 +289,89 @@ const flowsAlquiler = addKeyword(['//alqu-iler//'], {sensitive: true})
         .addAnswer('Selfie Mirror', {media: 'banner22.jpg'})
 
         .addAnswer('✈️ *Enviamos a todo el País*.', { capture: false }, async (ctx, { flowDynamic,gotoFlow, endFlow }) => {
- let dolar
-      await fetch('https://api.bluelytics.com.ar/v2/latest')
-.then(response => response.blue.value_sell)
-                motivo = "VENTA"         
-            //   numero(ctx.from);
-        
-await flowDynamic(`*VALOR ESPEJO MAGICO* \n
+          let dolar
+          await fetch('https://dolarapi.com/v1/dolares/blue')
+    .then(response => response.json())
+    .then(json => dolar = json.venta)
+                    motivo = "VENTA"       ;  
+                //   numero(ctx.from);
+            
+ await flowDynamic(`*VALOR ESPEJO MAGICO* \n
 💵   *U$D 1,500 .-*   🔒
 💱 > U$D = AR$ > 💱
 📈 AR$ ${new Intl.NumberFormat('es-MX').format(dolar*1500)} .-🔓 `);
     await  flowDynamic(`*VALOR PLATAFORMA 360*\n     
 💵   *U$D 1,500 .-*   🔒
 💱 > U$D = AR$ > 💱
-📈 AR$ ${new Intl.NumberFormat('es-MX').format(dolar*1500)} .-🔓
- \n 
-💱[1 U$S = AR ${dolar}.-]💱`);
-   console_log(ctx.from+dolar)
-    await gotoFlow(Cliente); 
-       endFlow()})
-  
+📈 AR$ ${new Intl.NumberFormat('es-MX').format(dolar*1500)} .-🔓`);
+ await flowDynamic(`Cotizacion actual: \n💱[1 U$S = AR ${dolar}.-]💱`);
+console.log('GOTOCLIENTE')
+await flowDynamic('Selfie Mirror 360 + Selfie',{media: 'dibu.jpg'})
+await flowDynamic('Showroom', {media: 'video.mp4', delay: 4000});
+await flowDynamic('Selfie Mirror', {media: 'video2.mp4'});
+await flowDynamic('Captura 360', {media: 'video360.mp4'});
+await flowDynamic("*ESTE CHAT AUTOMATICO FINALIZO.*", { 
+      capture: true,
+      buttons: [
+          {body: 'CONTINUAR CON AGENTE'},
+          {body: 'VOLVER AL MENU'},
+          {body: 'FINALIZAR'},
+      ],
+  }, async (ctx, { endFlow, gotoFlow, provider, flowDynamic}) => {
+          const mywhatsa = "5491140054474@s.whatsapp.net";
+
+if (ctx.body == 'CONTINUAR CON AGENTE') {
+
+await provider.sendtext(mywhatsa, `*${motivo}* \nNumero: +${ctx.from}\nNombre: *${ctx.pushName}*\nINFO: \n*${ctx.body}*`)
+  flowDynamic('UN AGENTE SE COMUNICARA CON USTED A LA BREVEDAD')
+await endFlow()
+
+} else if (ctx.body == 'VOLVER AL MENU') {
+
+ await gotoFlow(Menuflow)
+
+  } else if (ctx.body == 'FINALIZAR') {
+    flowDynamic('GRACIAS POR COMUNICARSE CON NOSOTROS. QUEDAMOS A SUS ORDENES.')
+await endFlow()
+}}
+)        
+   })
+//////////////////////////////////////////////////////////////// EVENTO WELCOME
+const organizadorflow = addKeyword('FLOWO¿¿RGAN',{sensitive:true})
+.addAnswer(['¡Optimiza tus espacios y atrae la atención de tus clientes con nuestros organizadores de fila con tecnología Pixel LED!\n',
+'En SELFIE MediaError, entendemos la importancia de mantener tus espacios organizados y atractivos. Nuestros organizadores de fila no solo te ayudarán a mantener un flujo ordenado de clientes, sino que también añadirán un toque de modernidad y estilo a tu negocio.\n',
+'¿Qué hace que nuestros organizadores de fila con tecnología Pixel LED sean especiales?\n',
+'✨ Iluminación espectacular: Nuestra tecnología Pixel LED ofrece una iluminación vibrante y personalizable que destacará tu marca y creará una experiencia memorable para tus clientes.\n',
+'🧹 Organización efectiva: Mantén tus filas en orden y evita la confusión con nuestros organizadores de alta calidad. ¡El caos será cosa del pasado!\n',
+'🎨 Personalización total: Personaliza la apariencia de tus organizadores para que se adapten a tu imagen corporativa o al tema de tu negocio.\n',
+'🌟 Destaca entre la multitud: Con nuestros organizadores de fila Pixel LED, tu negocio destacará en cualquier entorno, desde eventos, ferias comerciales hasta tiendas minoristas y restaurantes.\n',
+'¡Es el momento de darle a tu negocio una ventaja competitiva!\n',
+'¡Haz que tu negocio brille con nuestros organizadores de fila Pixel LED! 💫✨ #TecnologíaLED #OrganizaciónEfectiva #AtraeClientes\n',
+'*VALORES*\n',
+'*FILA VIP LED 80.000 AR$ C/U*\n',
+'*CADENA 1.5 mts $ 10.000 AR$*\n',
+'*SOGA 1.5 mts $ 20.000 AR$*\n',
+'*PRODUCCION ACTUAL (7/10 DIAS)*\n',
+'*POR FAVOR COMUNIQUESE AL +5491140054474 PARA CONTINUAR*'], {capture:false}, async (ctx, { endFlow, gotoFlow, provider, flowDynamic}) => {
+ flowDynamic('FILA VIP', {media: 'ledselfie.mp4'});
+await flowDynamic('FOTO FILA VIP', {media: '111.jpg'});
+gotoFlow(Menuflow);
+
+})
+
 
 //////////////////////////////////////////////////////////////// EVENTO WELCOME
 
 
   const flowPrincipal = addKeyword(EVENTS.WELCOME)  
-  .addAnswer("Hola, gracias por comunicarte con Selfie Mirror. Esta es una línea de respuestas automáticas. Responde con el número índice para continuar o continua al\n +5491140054474 - Nicolás", { capture: false }, async (ctx, { flowDynamic,gotoFlow, endFlow }) => {
-                    console.log(ctx.body) 
-                     await gotoFlow(Menuflow);
-                    endFlow()})
+  .addAnswer("Hola, gracias por comunicarte con Selfie Mirror. Esta es una línea de respuestas automáticas. Responde con el número índice para continuar o continua al\n +5491140054474 - Nicolás")
+    .addAnswer("Presentamos los nuevos organizadores de fila *FILA VIP* - Completamente unicos y originales. Otro producto innovador de Selfie Mirror.", {capture: true,
+    buttons: [
+        {body: 'INFO ORDENADORES LED'},
+        {body: 'CONTINUAR AL MENU'}    ]}, async (ctx, { fallBack, gotoFlow, provider, flowDynamic}) => {
+  if(ctx.body=='INFO ORDENADORES LED'){await gotoFlow(organizadorflow)}else
+                    {await gotoFlow(Menuflow)}
+         }       )
                     
 
 
@@ -217,15 +389,15 @@ await flowDynamic(`*VALOR ESPEJO MAGICO* \n
          delay: 5000 }, async (ctx, { fallBack, gotoFlow, provider, sock}) => {
     
     if (ctx.body == 'INFO. ALQUILER') {
-          gotoFlow(flowsAlquiler)
+      await   gotoFlow(flowsAlquiler)
       } else if (ctx.body == 'INFO. VENTA') {
-         gotoFlow(flowVenta)
+       await  gotoFlow(flowVenta)
         } else if (ctx.body == '+ OPCIONES') {
-            gotoFlow(Menuflow2)
+          await  gotoFlow(Menuflow2)
     }}
 ) 
 
-  const Menuflow2 = addKeyword(["me-nu"], { sensitive: true })
+  const Menuflow2 = addKeyword(["me-?nu"], { sensitive: true })
 
   
      .addAnswer("Menu", { 
@@ -263,20 +435,29 @@ await flowDynamic(`*VALOR ESPEJO MAGICO* \n
 
 
     const adapterDB = new JsonFileAdapter()
-    const adapterFlow = createFlow([flowPrincipal, flowVenta, flowsAlquiler, Cliente, Menuflow, audiono,Menuflow2])
+    const adapterFlow = createFlow([flowPrincipal, flowVenta, flowsAlquiler, Cliente, Menuflow, audiono,Menuflow2,organizadorflow])
     const adapterProvider = createProvider(MetaProvider, {
         jwtToken: 'EAAMziR3dWTwBOyI5iwUFZCeBqo2F3yZCvipXQlqUxlvtQkb122Sc91lLMJvZC72DobxvZBwO4lXWIdJ4FCTMISIqfpEPtxbWC9zkeffcbBU7W2Dn9cefzdRNDQEmdma9nxsmz6WfFKsK9Es7RwuZAteGov0mIZA0WPlusxgmmJNpcydS37cmjNa558ETrgfbIkQJJaba4Cv5ZCu8GZAe',
         numberId: '133862353148114',
-        verifyToken: '1532043378cb825c66b8c8bb670731b0',
+        verifyToken: 'asdasd',
         version: 'v16.0',
     })
 
-    createBot({
-        flow: adapterFlow,
-        provider: adapterProvider,
-        database: adapterDB,
-    })
-console_log(main.adapterDB)
-}
+   
+  const BotCreate = await createBot({
+    flow: adapterFlow,
+    provider: adapterProvider,
+    database: adapterDB,
+  });
+/** 
+  BotWrapper.initialize(BotCreate, {
+    CHATWOOT_URL: "https://chatwoot-production-36d7.up.railway.app/webhooks/whatsapp/+541166704322",
+    CHATWOOT_ID: "1",
+    CHATWOOT_INBOX_ID: "1",
+    CHATWOOT_API_ACCESS_TOKEN: "a19cc4f38d1c129044b376f5a89d90c7",
+  });
+*/
 
-main()
+};
+
+  main();
