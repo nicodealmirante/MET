3e3826aac6994ff21e65e877aa1624e90644ab80
nicodealmirante <nicolas.e.luna@gmail.com>
2023-10-27 03:35:28 -0300

    act

--- app.js
+++ app.js
@@ -282,7 +282,8 @@ const organizadorflow = addKeyword('FLOWO¿¿RGAN',{sensitive:true})
 '*PRODUCCION ACTUAL (7/10 DIAS)*\n',
 '*POR FAVOR COMUNIQUESE AL +5491140054474 PARA CONTINUAR*'])
 .addAnswer('FILA VIP', {media: 'ledselfie.mp4'})
-.addAnswer('FOTO FILA VIP', {media: '111.jpg'}, async (ctx, { gotoFlow, endFlow}) => {
+.addAnswer('FOTO FILA VIP', {media: '111.jpg'})
+addAction(async (ctx, { gotoFlow, endFlow}) => {
  return gotoFlow(Menuflow)
 })
 
