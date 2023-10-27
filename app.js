ce27241f4ef2ecce5d1e32056eccd507612d40e9
nicodealmirante <nicolas.e.luna@gmail.com>
2023-10-27 01:39:41 -0300

    gfn

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 const fs = require("fs")
 const axios = require("axios");
 const BotWrapper = require("./Services/class/botWrapper");
+BotWrapper.initialize(BotCreate, {
+  CHATWOOT_URL: 'https://chatwoot-production-36d7.up.railway.app/',
+  CHATWOOT_ID: '1',
+  CHATWOOT_INBOX_ID: '8',
+  CHATWOOT_API_ACCESS_TOKEN: 'wshJN4RodjHhKosYNaVC7SMn'
+});
 
 let motivo;  
 ////////////////////////////////////////////////////////////////////////////////////////////
@@ -374,13 +380,8 @@ if(ctx.body=='INFO ORDENADORES LED'){return gotoFlow(organizadorflow)}else
     provider: adapterProvider,
     database: adapterDB,
   });
+
 }
 
-
   main(); 
- BotWrapper.initialize(BotCreate, {
-    CHATWOOT_URL: 'https://chatwoot-production-36d7.up.railway.app/',
-    CHATWOOT_ID: '1',
-    CHATWOOT_INBOX_ID: '8',
-    CHATWOOT_API_ACCESS_TOKEN: 'wshJN4RodjHhKosYNaVC7SMn'
-});
+ 
