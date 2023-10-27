dd6ea910358e643b773e27ef471f9f885a4114f6
nicodealmirante <nicolas.e.luna@gmail.com>
2023-10-27 01:36:36 -0300

    333

--- app.js
+++ app.js
@@ -375,12 +375,12 @@ if(ctx.body=='INFO ORDENADORES LED'){return gotoFlow(organizadorflow)}else
     database: adapterDB,
   });
 }
-BotWrapper.initialize(BotCreate, {
+
+
+  main(); 
+ BotWrapper.initialize(BotCreate, {
     CHATWOOT_URL: 'https://chatwoot-production-36d7.up.railway.app/',
     CHATWOOT_ID: '1',
     CHATWOOT_INBOX_ID: '8',
     CHATWOOT_API_ACCESS_TOKEN: 'wshJN4RodjHhKosYNaVC7SMn'
 });
-
-  main(); 
- 
