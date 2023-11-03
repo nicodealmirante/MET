f430a9855920b40f3d459cc0c92ea873d3d7fdb5
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-03 01:56:22 -0300

    asddsaads

--- app.js
+++ app.js
@@ -553,7 +553,7 @@ return  gotoFlow(Menuflow);
       process.env.CHATWOOT_ID, process.env.CHATWOOT_URL, {
       accounts: 1,
     });
-    const adapterDB=MockAdapter
+    const adapterDB =new  MockAdapter()
     const adapterProvider = createProvider(MetaProvider, {
       jwtToken: 'EAAMziR3dWTwBOyI5iwUFZCeBqo2F3yZCvipXQlqUxlvtQkb122Sc91lLMJvZC72DobxvZBwO4lXWIdJ4FCTMISIqfpEPtxbWC9zkeffcbBU7W2Dn9cefzdRNDQEmdma9nxsmz6WfFKsK9Es7RwuZAteGov0mIZA0WPlusxgmmJNpcydS37cmjNa558ETrgfbIkQJJaba4Cv5ZCu8GZAe',
       numberId: '133862353148114',
