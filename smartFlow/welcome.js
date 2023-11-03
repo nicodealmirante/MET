3e239c4117496130d30e5808e6ebe1c57e75e840
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-03 19:24:58 -0300

    adsads

--- smartFlow/welcome.js
+++ smartFlow/welcome.js
@@ -9,22 +9,21 @@ module.exports = addKeyword(EVENTS.WELCOME)
    await   flowDynamic("Hola, gracias por comunicarte con Selfie Mirror. Esta es una línea de respuestas automáticas. Responde con el número índice para continuar o continua al\n +5491140054474 - Nicolás")
     await abc.createMessage({msg: "Hola, gracias por comunicarte con Selfie Mirror. Esta es una línea de respuestas automáticas. Responde con el número índice para continuar o continua al\n +5491140054474 - Nicolás ", mode: "outgoing"})
       await abc.createMessage({msg: "Opciones \n INFO. ALQUILER\nINFO. VENTA\n UNIFILA LED", mode: "outgoing"})
-      const dataINI = {msg: "Opciones \n HABLAR CON ASESOR\nINFO DE LA EMPRESA\n PAGINA WEB", mode: "outgoing"}
-      await abc.createMessage({msg: opc, mode: "outgoing"})},
         await  flowDynamic("Opciones", {capture: false, 
               buttons: [
           {body: 'INFO. ALQUILER'},
           {body: 'INFO. VENTA'},
           {body: 'UNIFILA LED'}, 
-      ], delay: 2000 }),
+      ], delay: 2000 })
+
 await     flowDynamic(
-        "*Contacto*", {  capture: true,
+        "*Contacto*", { capture: true,
               buttons: [
           {body: 'HABLAR CON ASESOR'},
           {body: 'INFO DE LA EMPRESA'},
           {body: 'PAGINA WEB'},
-      ],    delay: 3000 
-    }, async (ctx, {flowDynamic}) => {
+      ],    delay: 3000 }
+    )    
         const dataIn= {msg: ctx.body, mode: "incoming"}
         const abc = new ChatWood()
       await abc.createMessage(dataIn)
@@ -47,5 +46,5 @@ if (ctx.body == 'PAGINA WEB') {
     msj = ('SELFIE MIRROR \nhttps://www.espejoselfiemirror.com.ar\n FILA VIP \nhttps://filavip.ar')      
     } else if (ctx.body == 'INFO DE LA EMPRESA') {
         msj = ('*Av de Mayo 1624  - RAMOS MEJÍA - Buenos Aires\n Nuestros horarios de atención son: de Lunes a Viernes de 10hs a 17hs' )
-    }}}))
+    }}})
    
