bd5fcacdd45bb0c6c74cd597caf0e8eec5675294
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-03 19:46:53 -0300

    asd

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const MockAdapter = require("@bot-whatsapp/database/mock");
 //----------------- CHATWOOT
 const ServerAPI = require("./http");
 const ChatWood = require("./http/services/chatwood.js");
+const flowPrincipal = require("./smartFlow/welcome");
 
 
 /** * Aqui declaramos los flujos hijos, los flujos se declaran de atras para adelante, es decir que si tienes un flujo de este tipo:
@@ -372,7 +373,6 @@ return  gotoFlow(Menuflow);
 */
  
 
-const flowPrincipal = require("./smartFlow/welcome");
 
 /**
   const flowPrincipal = addKeyword(EVENTS.WELCOME)
