c8496541a70a78c6242c9bf755c7d8365a09b11a
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-03 03:20:12 -0300

    ads

--- provider/database/index.js
+++ provider/database/index.js
@@ -1,9 +1,9 @@
-const MongoAdapter = require("./mongo");
+const MockAdapter = require("./mock");
 
 const MONGO_DB_URI = process.env.MONGO_DB_URI;
 const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
 
-const adapterDB = new MongoAdapter({
+const adapterDB = new MockAdapter({
   dbUri: MONGO_DB_URI,
   dbName: MONGO_DB_NAME,
 });
