d1ca47165aae58e84758bb7a636a36b323a2802b
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-03 19:17:46 -0300

    dfsdfs

--- smartFlow/welcome.js
+++ smartFlow/welcome.js
@@ -6,17 +6,16 @@ module.exports = addKeyword(EVENTS.WELCOME)
         const dataIn= {msg: ctx.body, mode: "incoming"}
         const abc = new ChatWood()
       await abc.createMessage(dataIn)
-      flowDynamic("Hola, gracias por comunicarte con Selfie Mirror. Esta es una línea de respuestas automáticas. Responde con el número índice para continuar o continua al\n +5491140054474 - Nicolás")
+   await   flowDynamic("Hola, gracias por comunicarte con Selfie Mirror. Esta es una línea de respuestas automáticas. Responde con el número índice para continuar o continua al\n +5491140054474 - Nicolás")
     await abc.createMessage({msg: "Hola, gracias por comunicarte con Selfie Mirror. Esta es una línea de respuestas automáticas. Responde con el número índice para continuar o continua al\n +5491140054474 - Nicolás ", mode: "outgoing"})
       await abc.createMessage({msg: "Opciones \n INFO. ALQUILER\nINFO. VENTA\n UNIFILA LED", mode: "outgoing"})
       const dataINI = {msg: "Opciones \n HABLAR CON ASESOR\nINFO DE LA EMPRESA\n PAGINA WEB", mode: "outgoing"}
-      await abc.createMessage({msg: opc, mode: "outgoing"})})
-
-          flowDynamic("Opciones", {capture: false, 
+      await abc.createMessage({msg: opc, mode: "outgoing"})},
+        await  flowDynamic("Opciones", {capture: false, 
               buttons: [
           {body: 'INFO. ALQUILER'},
           {body: 'INFO. VENTA'},
-          {body: 'UNIFILA LED'},
+          {body: 'UNIFILA LED'}, 
       ], delay: 2000 }),
      flowDynamic(
         "*Contacto*", {  capture: true,
@@ -48,5 +47,5 @@ if (ctx.body == 'PAGINA WEB') {
     msj = ('SELFIE MIRROR \nhttps://www.espejoselfiemirror.com.ar\n FILA VIP \nhttps://filavip.ar')      
     } else if (ctx.body == 'INFO DE LA EMPRESA') {
         msj = ('*Av de Mayo 1624  - RAMOS MEJÍA - Buenos Aires\n Nuestros horarios de atención son: de Lunes a Viernes de 10hs a 17hs' )
-    }}})
+    }}}))
    
