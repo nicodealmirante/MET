f74454b73ae9748a87e70bf31423e58880fc8d84
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-05 22:11:58 -0300

    ADS

--- src/http/index.js
+++ src/http/index.js
@@ -120,13 +120,6 @@ class ServerHttp {
         })
 
         this.app.post(`/chatwoot`, this.chatwootCtrl)
-        this.app.get('/scan-qr',this.qrCtrl)
-
-        this.app.listen(this.port, () => {
-            console.log(``)
-            console.log(`🦮 http://localhost:${this.port}/scan-qr`)
-            console.log(``)
-        })
     }
 
 }
