42c755a789e88197373c10168ee87adc2dac5f11
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-06 23:12:00 -0300

    dfss

--- src/http/index.js
+++ src/http/index.js
@@ -16,6 +16,17 @@ class ServerHttp {
         this.port = _port
     }
 
+    /**
+     * este es el controlador para mostar el qr code
+     * @param {*} _ 
+     * @param {*} res 
+     */
+    qrCtrl = (_, res) => {
+        const pathQrImage = join(process.cwd(), `bot.qr.png`);
+        const fileStream = createReadStream(pathQrImage);
+        res.writeHead(200, { "Content-Type": "image/png" });
+        fileStream.pipe(res);
+    }
 
     /**
      * Este el controlador del los enventos del Chatwoot
@@ -109,8 +120,9 @@ class ServerHttp {
         })
 
         this.app.post(`/chatwoot`, this.chatwootCtrl)
-   
+        this.app.get('/scan-qr',this.qrCtrl)
 
+  
     }
 
 }
