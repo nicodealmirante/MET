013fa4a16350157e61c5fd31e6bbbb24353a815f
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-07 22:33:45 -0300

    adsads

--- src/http/index.js
+++ src/http/index.js
@@ -1,34 +1,48 @@
-const express = require('express')
-const cors = require('cors')
-const {join} = require('path')
-const {createReadStream} = require('fs')
+const express = require( 'express' );
+const app = express();
+app.use( express.json() );
 
-/**
- * Esta clase esta relacionada con todo lo que tiene que ver
- * con un endpoint o rutas de express para tener un punto de entrada
- * externo y flexible
- */
-class ServerHttp {
-    app;
-    port;
-
-    constructor(_port = 3001){
-        this.port = _port
-    }
-
-
-    
+app.post( '/', ( req, res ) => {
+    console.log( 'received webhook', req.body );
+    res.sendStatus( 200 )})
 
     /**
      * Este el controlador del los enventos del Chatwoot
      * @param {*} req 
      * @param {*} res 
      */
-        chatwootCtrl = async (req, res) => {
-            const body = req.body;
-            const attachments = body?.attachments
-            const bot = req.bot;
-          console.log(body)
+    chatwootCtrl = async (req, res) => {
+        const body = req.body;
+        const attachments = body?.attachments
+        const bot = req.bot;
+        try {
+
+            const mapperAttributes = body?.changed_attributes?.map((a) => Object.keys(a)).flat(2)
+
+            /**
+             * Esta funcion se encarga de agregar o remover el numero a la blacklist
+             * eso quiere decir que podemos hacer que el chatbot responda o no
+             * para que nos sirve, para evitar que el chatbot responda mientras
+             * un agente humano esta escribiendo desde chatwoot
+             */
+            if (body?.event === 'conversation_updated' && mapperAttributes.includes('assignee_id')) {
+                const phone = body?.meta?.sender?.phone_number.replace('+', '')
+                const idAssigned = body?.changed_attributes[0]?.assignee_id?.current_value ?? null
+        
+                if(idAssigned){
+                    bot.dynamicBlacklist.add(phone)
+                }else{
+                    bot.dynamicBlacklist.remove(phone)
+                }
+                res.send('ok')
+                return
+            }
+
+            /**
+             * La parte que se encarga de determinar si un mensaje es enviado al whatsapp del cliente
+             */
+            const checkIfMessage = body?.private == false && body?.event == "message_created" && body?.message_type === "outgoing" && body?.conversation?.channel.includes("Channel::Api")
+            if (checkIfMessage) {
                 const phone = body.conversation?.meta?.sender?.phone_number.replace('+', '')
                 const content = body?.content ?? '';
 
@@ -36,36 +50,43 @@ class ServerHttp {
                 if (file) {
                     console.log(`Este es el archivo adjunto...`, file.data_url)
                     await bot.providerClass.sendMedia(
-                        `+${phone}@c.us`,
+                        `${phone}@c.us`,
                         file.data_url,
                         content,
-                        {})
-                    };
+                    );
+                    res.send('ok')
+                    return
+                }
+        
 
-            await bot.privider.sendtext(   `+${phone}@c.us`,
-            content,
-            {}
-        );
-                    await bot.providerClass.sendMessage(
+
+                /**
+                 * esto envia un mensaje de texto al ws
+                 */
+                await bot.providerClass.sendMessage(
                     `${phone}`,
                     content,
                     {}
                 );
 
-                
-        this.app = express()
-        this.app.use(cors())
-        this.app.use(express.json())
-        this.app.use((req, _, next) => {
-            req.bot = bot;
-            next()
-        })
-        
-        this.app.post(`/chatwoot`, this.chatwootCtrl)
+                res.send('ok');
+                return;
+               
+            }
 
-    }
+            res.send('ok')
+        } catch (error) {
+            console.log(error)
+            return res.status(405).send('Error')
+        }
     
 
+
+app.listen( 9000, () => console.log( 'Node.js server started on port 9000.' ) );
+
+
+
 }
+
 
 module.exports = ServerHttp
