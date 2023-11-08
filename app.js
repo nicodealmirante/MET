d73e417bb9a39f6584936f627b20146246694463
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-08 16:25:14 -0300

    SADADS

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const ordenadores = organizadores("./flow/ordenadores");
 
 
 
- 
+
 
  const MENU = addKeyword('MENU')
 
@@ -83,7 +83,7 @@ const ordenadores = organizadores("./flow/ordenadores");
  /////////////////  FLUJO MENU
   
  if (ctx.body == 'CONTINUAR CON ASESOR') {
-  await adapterProvider.sendMessage(mywhatsa, `*VENTA* \nNumero: +${ctx.from}\nNombre: *${ctx.pushName}*\nINFO: \n*${ctx.body}*`,{})
+  await adapterProvider.sendMessage(mywhatsa, `*VENTA* \nNumero: +${ctx.from}\nNombre: *${ctx.pushName}*\nINFO: \n*${ctx.body}*`)
 
 await  flowDynamic('UN AGENTE SE COMUNICARA CON USTED A LA BREVEDAD')
 
