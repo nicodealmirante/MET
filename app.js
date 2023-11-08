acbf5f6e8c673fe9f02328b3c793257de728aa11
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-08 17:03:12 -0300

    dasads

--- app.js
+++ app.js
@@ -1,175 +1,126 @@
-require('dotenv').config()
+equire('dotenv').config()
 const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
 const Queue = require('queue-promise')
-const MetaProvider = require("@bot-whatsapp/provider/meta")
+const MetaProvider = require('@bot-whatsapp/provider/meta')
 const MockAdapter = require('@bot-whatsapp/database/mock')
-const ServerHttp = require('./src/http')
 
 const ChatwootClass = require('./src/chatwoot/chatwoot.class')
 const { handlerMessage } = require('./src/chatwoot')
-const  PORTS = 3004
-let motivo;  
-const mywhatsa = "+5491140054474@s.whatsapp.net";
 
+const mywhatsa = "+5491140054474@s.whatsapp.net"
 
-const venta = require("./flows/venta");
-const alquiler = require("./flows/alquiler");
-const ordenadores = require("./flows/ordenadores");
+const venta = require("./flows/venta")
+const alquiler = require("./flows/alquiler")
+const ordenadores = require("./flows/ordenadores")
 
+const audiono = addKeyword(EVENTS.VOICE_NOTE)
+  .addAnswer('Disculpe, no puedo escuchar audios. Por favor utilice solo texto.')
+  .addAction(async (ctx, { gotoFlow, endFlow }) => {
+    return endFlow(Menuflow)
+  })
 
-///////////////////////////////////// XXXXXXXXXXXXXXXXXXXXX ////////////////////////////
-                         /////////         EVENTO VOICE    ///////////////////
-/////////////////////////////////////  XXXXXXXXXXXXXXXXXXX   /////////////////////////////////////
+const flowPrincipal = addKeyword(EVENTS.WELCOME)
+  .addAnswer("Hola, Gracias por comunicarse. Mi nombre es Luna, Soy una recepcionista virtual. Estoy configurada para brindarle información completa e inmediata. En el siguiente menú puede obtener información detallada sobre nuestros productos.")
+  .addAnswer("También puede dejarnos su teléfono o comunicarse con un asesor al *+5491140054474 - NICOLAS*")
+  .addAction(async (ctx, { gotoFlow }) => {
+    gotoFlow(Menuflow)
+  })
 
+const Menuflow = addKeyword('MENU')
+  .addAnswer("*INFORMACION AUTOMATICA*", {
+    capture: false,
+    buttons: [
+      { body: '📙 INFO. ALQUILER 📙' },
+      { body: '📎 INFO. VENTA 📎' },
+      { body: 'PRECIOS' },
+    ],
+    delay: 2000
+  })
+  .addAnswer("*Contacto*", {
+    capture: true,
+    buttons: [
+      { body: 'HABLAR CON ASESOR' },
+      { body: 'INFO DE LA EMPRESA' },
+      { body: 'PAGINA WEB' },
+    ],
+    delay: 3000
+  })
+  .addAnswer('SELFIE MIRROR \nhttps://www.espejoselfiemirror.com.ar')
+  .addAnswer('FILA VIP \nhttps://filavip.ar')
+  .addAction(async (ctx, { gotoFlow }) => {
+    gotoFlow(Menuflow)
+  })
 
-  const audiono = addKeyword(EVENTS.VOICE_NOTE)
-              .addAnswer('Disculpe, no puedo escuchar audios. Por favor utilice solo texto.')
-               .addAction(async(ctx, {gotoFlow,endFlow}) => { 
-                    return endFlow(Menuflow)})
-
-///////////////////////////////////// XXXXXXXXXXXXXXXXXXXXX ////////////////////////////
-                         /////////         FLUJO ALQUILER     ///////////////////
-/////////////////////////////////////  XXXXXXXXXXXXXXXXXXX   /////////////////////////////////////
-
-
-///////////////////////////////////// XXXXXXXXXXXXXXXXXXXXX ////////////////////////////
-                         /////////     FLUJO VENTA UNIFILA     ///////////////////
-/////////////////////////////////////  XXXXXXXXXXXXXXXXXXX   /////////////////////////////////////
-
-
-       
-                     
-///////////////////////////////////// XXXXXXXXXXXXXXXXXXXXX ////////////////////////////
-                         /////////     FLUJO VENTA UNIFILA     ///////////////////
-/////////////////////////////////////  XXXXXXXXXXXXXXXXXXX   /////////////////////////////////////
-
-
-  
-//////////////////////////////////////////////////////////////// EVENTO WELCOME
-
-  const flowPrincipal = addKeyword(EVENTS.WELCOME) 
-  
-  .addAnswer("Hola, Gracias por comunicarse. Mi nombre es Luna, Soy una recepcionista virtual. Estoy configurada para brindarle informacion completa e inmediata. En el siguiente menu puede obtener informacion detallada sobre nuestros productos.")
- .addAnswer("Tambien puede dejarnos su telefono o comunicarse con un asesor al *+5491140054474 - NICOLAS*")
- .addAction(async (ctx, {gotoFlow}) => {gotoFlow(Menuflow)})
-
-
-
-
-
-
- const Menuflow = addKeyword('MENU')
-
-  .addAnswer("*INFORMACION AUTOMATICA*", {capture: false, 
-            buttons: [
-                {body: '📙 INFO. ALQUILER 📙'},
-                {body: '📎 INFO. VENTA 📎'},
-                {body: 'PRECIOS'},
-            ], delay: 2000 }) 
-                   
-     .addAnswer("*Contacto*", {capture: true,
-        buttons: [
-          {body: 'HABLAR CON ASESOR'},
-          {body: 'INFO DE LA EMPRESA'},
-          {body: 'PAGINA WEB'},
-      ], delay: 3000 })
-   
-   .addAnswer('SELFIE MIRROR \nhttps://www.espejoselfiemirror.com.ar')  
-   .addAnswer('FILA VIP \nhttps://filavip.ar')  
-   .addAction(async (ctx, {gotoFlow}) => {gotoFlow(Menuflow)
-
-   
-          
- /////////////////  FLUJO MENU
-  
- if (ctx.body == 'CONTINUAR CON ASESOR') {
-   adapterProvider.sendMessage(mywhatsa, `*VENTA* \nNumero: +${ctx.from}\nNombre: *${ctx.pushName}*\nINFO: \n*${ctx.body}*`)
-
-await  flowDynamic('UN AGENTE SE COMUNICARA CON USTED A LA BREVEDAD')
-
-return gotoFlow(Menuflow)
- } else 
-if (ctx.body == 'PAGINA WEB') {
-  await flowDynamic('SELFIE MIRROR \nhttps://www.espejoselfiemirror.com.ar')  
-  await flowDynamic('FILA VIP \nhttps://filavip.ar')  
-    gotoFlow(Menuflow);
+// Lógica para procesar opciones en el menú
+if (ctx.body == 'CONTINUAR CON ASESOR') {
+  adapterProvider.sendMessage(mywhatsa, `*VENTA* \nNumero: +${ctx.from}\nNombre: *${ctx.pushName}*\nINFO: \n*${ctx.body}*`, {})
+  flowDynamic('UN AGENTE SE COMUNICARÁ CON USTED A LA BREVEDAD')
+  gotoFlow(Menuflow)
+} else if (ctx.body == 'PAGINA WEB') {
+  flowDynamic('SELFIE MIRROR \nhttps://www.espejoselfiemirror.com.ar')
+  flowDynamic('FILA VIP \nhttps://filavip.ar')
+  gotoFlow(Menuflow)
 } else if (ctx.body == 'INFO DE LA EMPRESA') {
-  await flowDynamic('En [Nombre de Tu Empresa], nos enorgullece ser pioneros en la industria. Desde nuestros inicios en 2018, fuimos los primeros en fabricar el revolucionario Espejo Mágico y la experiencia 360 en Argentina. Además, nuestras innovadoras Unifilas LED han establecido un estándar global en la industria. Con un compromiso constante con la calidad y la creatividad, estamos aquí para hacer que tus eventos sean inolvidables.')
-await flowDynamic('*Av de Mayo 1624  - RAMOS MEJÍA - Buenos Aires*' )
-await flowDynamic('  Nuestros horarios de atención son: de Lunes a Viernes de 10hs a 17hs' )
+  flowDynamic('En [Nombre de Tu Empresa], nos enorgullece ser pioneros en la industria. Desde nuestros inicios en 2018, fuimos los primeros en fabricar el revolucionario Espejo Mágico y la experiencia 360 en Argentina. Además, nuestras innovadoras Unifilas LED han establecido un estándar global en la industria. Con un compromiso constante con la calidad y la creatividad, estamos aquí para hacer que tus eventos sean inolvidables.')
+  flowDynamic('*Av de Mayo 1624  - RAMOS MEJÍA - Buenos Aires*')
+  flowDynamic('Nuestros horarios de atención son: de Lunes a Viernes de 10hs a 17hs')
+  flowDynamic('Selfie Mirror', { media: 'video.mp4' })
+  gotoFlow(Menuflow)
+}
 
-await flowDynamic('Selfie Mirror', {media: 'video.mp4'})}
+const chatwoot = new ChatwootClass({
+  account: process.env.account,
+  token: process.env.token,
+  endpoint: process.env.endpoint,
+})
 
-return  gotoFlow(Menuflow);})
+const queue = new Queue({
+  concurrent: 1,
+  interval: 500
+})
 
+const main = async () => {
+  const adapterDB = new MockAdapter()
+  const adapterFlow = createFlow([alquiler, venta, ordenadores, Menuflow, audiono])
 
-        
+  const adapterProvider = createProvider(MetaProvider, {
+    jwtToken: process.env.jwtToken,
+    numberId: process.env.numberId,
+    verifyToken: process.env.verifyToken,
+    version: 'v18.0'
+  })
 
-////////////////////////////////////////////////////////////////////////////////////////
+  const bot = await createBot({
+    flow: adapterFlow,
+    provider: adapterProvider,
+    database: adapterDB,
+  })
 
+  // ServerHttp.initialization(bot) - Esto debe ser descomentado si deseas habilitar un servidor HTTP
 
-
-    const chatwoot = new ChatwootClass({
-        account: process.env.account,
-        token: process.env.token,
-        endpoint: process.env.endpoint,
-        
+  // Los mensajes entrantes al bot (cuando el cliente nos escribe)
+  adapterProvider.on('message', (payload) => {
+    queue.enqueue(async () => {
+      await handlerMessage({
+        phone: payload.from,
+        name: payload.pushName,
+        message: payload.body,
+        mode: 'incoming'
+      }, chatwoot)
     })
-    
-    const queue = new Queue({
-        concurrent: 1,
-        interval: 500
+  })
+
+  // Los mensajes salientes (cuando el bot le envía un mensaje al cliente)
+  bot.on('send_message', (payload) => {
+    queue.enqueue(async () => {
+      await handlerMessage({
+        phone: payload.numberOrId,
+        name: payload.pushName,
+        message: payload.answer,
+        mode: 'outgoing'
+      }, chatwoot)
     })
-    
-    const main = async () => {
-        const adapterDB = new MockAdapter()
-        const adapterFlow = createFlow([alquiler, venta, ordenadores, Menuflow, audiono])
+  })
+}
 
-        const adapterProvider = createProvider(MetaProvider, {
-          
-          
-          jwtToken: process.env.jwtToken,
-          numberId: process.env.numberId,
-          verifyToken: process.env.verifyToken,
- version: 'v18.0'
-        })
-          
-        const bot = await createBot({
-            flow: adapterFlow,
-            provider: adapterProvider,
-            database: adapterDB,
-        })
-    
-   ///     ServerHttp.initialization(bot)
-        /**
-         * Los mensajes entrantes al bot (cuando el cliente nos escribe! <---)
-         */
-    
-        adapterProvider.on('message', (payload) => {
-            queue.enqueue(async () => {
-                await handlerMessage({
-                    phone:payload.from, 
-                    name:payload.pushName,
-                    message: payload.body, 
-                    mode:'incoming'
-                }, chatwoot)
-            });
-        })
-    
-        /**
-         * Los mensajes salientes (cuando el bot le envia un mensaje al cliente ---> )
-         */
-        bot.on('send_message', (payload) => {
-            queue.enqueue(async () => {
-                await handlerMessage({
-                    phone:payload.numberOrId, 
-                    name:payload.pushName,
-                    message: payload.answer, 
-                    mode:'outgoing'
-                }, chatwoot)
-            })
-        })
-
-    }
-    
-    main()
+main()
