2d744c9185ad75e547f6e9d42f173badff23cf76
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-13 02:51:00 -0300

    das

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Queue = require('queue-promise')
 const MetaProvider = require("@bot-whatsapp/provider/meta")
 const MockAdapter = require('@bot-whatsapp/database/mock')
 const ServerHttp = require('./src/http')
+const cors = require('cors')
 
 const ChatwootClass = require('./src/chatwoot/chatwoot.class')
 const { handlerMessage } = require('./src/chatwoot')
