e7165862a58bcce3efe9bfc4f0c34261f374c29b
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-12 21:28:54 -0300

    trytyr

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { handlerMessage } = require('./src/chatwoot')
 const  PORT = 3004 
 let motivo;  
 const mywhatsa = "5491140054474";
-const ServerHttp = new ServerHttp(PORT)
+const serverHttp = new ServerHttp(PORT)
 /** * Aqui declaramos los flujos hijos, los flujos se declaran de atras para adelante, es decir que si tienes un flujo de este tipo:
  *
  *          Menu Principal
