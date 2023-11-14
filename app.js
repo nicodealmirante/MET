5fe917d3990568ac1d2b01b42f5d341428b0ab5e
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-13 22:49:46 -0300

    iukkufdsfs

--- app.js
+++ app.js
@@ -623,6 +623,8 @@ return  gotoFlow(Menuflow);
         
  */
 ////////////////////////////////////////////////////////////////////////////////////////
+const mensaje = addKeyword(["mennnn"], { sensitive: true })
+
   .addAnswer("*Info*", { 
             capture: true,
             buttons: [
@@ -664,21 +666,7 @@ return  gotoFlow(Menuflow);
             database: adapterDB,
         })
     
-<<<<<<< HEAD
     }
-=======
-      
-    
-   ///     ServerHttp.initialization(bot)
-        /**
-         * Los mensajes entrantes al bot (cuando el cliente nos escribe! <---)
-         */
-    
-
-    }  
-    main()
-    
->>>>>>> 7cfc7482322fefb347bf5b8ec65d91d38d368568
 
 const mainb = async () => {
   const BOTNAME = 'botbai' 
@@ -691,7 +679,7 @@ const mainb = async () => {
         flow: adapterFlow,
         provider: adapterProvider,
         database: adapterDB,
-    })  
+    })
 
     serverHttp.initialization(bot)
 
@@ -725,4 +713,4 @@ const mainb = async () => {
       })
 mainb();
     
-  
+    main()
