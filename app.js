4017bc18dce44a9ca1ae2631c1c66a4d1741e35d
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-14 13:28:36 -0300

    dfgdfgdfgdfg

--- app.js
+++ app.js
@@ -66,12 +66,17 @@ const Cliente = addKeyword(["ASESOR VENTAS"],{sensitive:true})
 .addAnswer('ESTA CONVERSACION FINALIZO')
 .addAnswer('Para continuar con asesor haga click en el siguiente enlace')
 .addAnswer('https://wa.me/5491140054474?text=ASESOR',{capture:true, delay:5000}, async (ctx ,{createProvider,adapterProviderb,BaileysProvider}) => {
-  const adapterProviderb = createProvider(BaileysProvider);
+
+  await axios(`http://api.railway.internal:3000/enviar-mensaje/:${ctx.from}`)
+    await adapterProviderb.sendText("5491159132301@s.whatsapp.net", "mensaje");
+
+  return fallBack("CONTINUE AQUI --->> https://wa.me/5491140054474?text=ASESOR")
+
+  
+  });
  
-await adapterProviderb.sendText("5491159132301@s.whatsapp.net", "mensaje");
 
 
-})
 /** 
 })
          
