caa77964609ee9217fc750a5cb8c118d23f7a1b0
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-15 01:16:48 -0300

    adsadsads

--- app.js
+++ app.js
@@ -57,22 +57,12 @@ console.log('Numero Agendado de Alquiler');*/
 /////////////////////// FLUJO CLIENTE
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Creamos un nuevo XMLHttpRequest
-var xhttp = new XMLHttpRequest();
+
 
 // Esta es la función que se ejecutará al finalizar la llamada
-xhttp.onreadystatechange = function() {
-  // Si nada da error
-  if (this.readyState == 4 && this.status == 200) {
-    // La respuesta, aunque sea JSON, viene en formato texto, por lo que tendremos que hace run parse
-    console.log(JSON.parse(this.responseText));
-  }
-};
 
-// Endpoint de la API y método que se va a usar para llamar
-xhttp.open("GET", "https://api-production-34a0.up.railway.app/enviar-mensaje", true);
-xhttp.setRequestHeader("Content-type", "application/json");
-// Si quisieramos mandar parámetros a nuestra API, podríamos hacerlo desde el método send()
-xhttp.send(null);
+
+
 const Cliente = addKeyword(["ASESOR VENTAS"],{sensitive:true})
 .addAnswer('ESTA CONVERSACION FINALIZO')
 .addAnswer('Haga click en el siguiente enlace para continuar')
