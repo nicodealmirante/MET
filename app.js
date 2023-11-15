423ec3e1ab897c8b86390c5a01b2825aa5c5a9c5
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-15 14:19:15 -0300

    Update app.js
    Signed-off-by: nicodealmirante <nicolas.e.luna@gmail.com>

--- app.js
+++ app.js
@@ -63,7 +63,6 @@ const Cliente = addKeyword(["ASESOR VENTAS"],{sensitive:true})
   .addAnswer('+5491140054474 - NICOLAS SE COMUNICARA CON USTED',{capture: false,
        idle: 200000 }, // idle: 2000 = 2 segundos
       async (ctx, { gotoFlow, inRef,provider }) => {
-     await provider.sendtext(mywhatsa, `${causa} ${ctx.from}`)
      await provider.sendtext(mywhatsa, `*${causa}* \nNumero: +${ctx.from}\nNombre: *${ctx.pushName}*\nINFO: \n*${ctx.body}*`)
 
      
@@ -690,7 +689,7 @@ return  gotoFlow(Menuflow);
             })
         })
 
-    
+    main()
 
     }
     
