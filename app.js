31a24e35ffa9c2f3968d78c637b1f741b55de6f8
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-22 05:30:40 -0300

    Update app.js
    Signed-off-by: nicodealmirante <nicolas.e.luna@gmail.com>

--- app.js
+++ app.js
@@ -639,7 +639,7 @@ return  gotoFlow(Menuflow);
       createDashboard({
         CHATWOOT_URL: "https://chatwoot-production-9374.up.railway.app",
         CHATWOOT_ID: "1",
-        CHATWOOT_INBOX_ID: "4",
+        CHATWOOT_INBOX_ID: "6",
         CHATWOOT_API_ACCESS_TOKEN: "RzqiiFrYqQUrx5FPuuMXoM3e"
       }, BotCreate)
 
