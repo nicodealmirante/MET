dd5b73988abf29724442c260866bde2a69898e01
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-25 04:42:48 -0300

    dsffds

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.post('/enviar-mensaje', async (req, res) => {
   await provider.sendtext(`${phone}@c.us`,message)
 
   res.send({phone, message})
-
 })
 const flowflow = addKeyword("hi").addAction(async(ctx,{adapterProvider}) =>{
 })
@@ -35,7 +34,7 @@ const main = async () => {
     jwtToken: "EAAMziR3dWTwBOyI5iwUFZCeBqo2F3yZCvipXQlqUxlvtQkb122Sc91lLMJvZC72DobxvZBwO4lXWIdJ4FCTMISIqfpEPtxbWC9zkeffcbBU7W2Dn9cefzdRNDQEmdma9nxsmz6WfFKsK9Es7RwuZAteGov0mIZA0WPlusxgmmJNpcydS37cmjNa558ETrgfbIkQJJaba4Cv5ZCu8GZAe",
     numberId: "133862353148114",
         verifyToken: 'asdasd',
-        version: 'v18.0'})
+        version: 'v18.0'})}
 
   createBot({
     flow: adapterFlow,
@@ -45,23 +44,18 @@ const main = async () => {
 const app = express();
 
   const PORT = process.env.PORT || 4000;
-  app.listen(3005, () => console.log(`http://localhost:${3005}`));
-  app.post('/enviar-mensaje', async (req, res) => {
+  app.listen(3005, () => {
 
-    const adapterProvider = req.ws;
-  const body = req.body
-  const message = body.content
-  const phone = body.conversation.meta.sender.phone_number.replace('+','')
-  console.log(`${phone} asd ${body}`)
-  await provider.sendtext(`${phone}@c.us`,message)
-
-  res.send({phone, message})
-
-})
-
-
-
-
-};
-
-main();
+  const adapterProvider = req.ws;
+const body = req.body
+const message = body.content
+const phone = body.conversation.meta.sender.phone_number.replace('+','')
+console.log(`${phone} asd ${body}`)
+await provider.sendtext(`${phone}@c.us`,message)
+  
+res.send({phone, message})
+    
+  
+  })
+  
+main()
