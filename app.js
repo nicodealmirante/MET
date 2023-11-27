517623961d8a74a1ce8978c36aa44640a45db9a8
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-26 22:43:29 -0300

    fdsdf

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const { createBot, createProvider, createFlow, addKeyword, EVENTS, ProviderClass
 const Queue = require('queue-promise')
 const MetaProvider = require("@bot-whatsapp/provider/meta")
 const MockAdapter = require('@bot-whatsapp/database/mock')
-const express = require('express')
+const ServerHttp = require('./src/http')
+
 const ChatwootClass = require('./src/chatwoot/chatwoot.class')
 const { handlerMessage } = require('./src/chatwoot')
+const  PORTS = 3004 
 let motivo;  
 
 /** * Aqui declaramos los flujos hijos, los flujos se declaran de atras para adelante, es decir que si tienes un flujo de este tipo:
@@ -647,14 +649,13 @@ return  gotoFlow(Menuflow);
           version: 'v18.0'})
         
           
-         const bot = await  createBot({
+        const bot = await createBot({
             flow: adapterFlow,
             provider: adapterProvider,
             database: adapterDB,
-          });
-        const app = express();
+        })
     
-    
+      const serverHttp=ServerHttp(bot)
         /**
          * Los mensajes entrantes al bot (cuando el cliente nos escribe! <---)
          */
@@ -683,20 +684,40 @@ return  gotoFlow(Menuflow);
                 }, chatwoot)
             })
         })
-        app.get(`/smisd`, async (req, res) => {
-          const id = "5491159132301@c.us";
-                    const templateMessage = {
-            text: "Hi it's a template message"
-          };
-      
-          const abc = await adapterProvider.getInstance();
-          await abc.sendMessage(id, templateMessage);
-      
-          res.send({ data: req.params.isd });
-        });
-      
-        const PORT = process.env.PORT || 4000;
-        app.listen(3004, () => console.log(`http://localhost:${PORT}`));
-      };
+        const id = "5491159132301@c.us";
+        const templateButtons = [
+      {
+        index: 1,
+        urlButton: {
+          displayText: ":star: Star Baileys on GitHub!",
+          url: "https://github.com/adiwajshing/Baileys",
+        },
+      },
+      {
+        index: 2,
+        callButton: {
+          displayText: "Call me!",
+          phoneNumber: "+1 (234) 5678-901",
+        },
+      },
+      {
+        index: 3,
+        quickReplyButton: {
+          displayText: "This is a reply, just like normal buttons!",
+          id: "id-like-buttons-message",
+        },
+      },
+    ];
+
+    const templateMessage = {
+      text: "Hi it's a template message"
+    };
+
+    const abc = await adapterProvider.getInstance();
+    await abc.sendMessage(id, templateMessage);
+
+    res.send({ data: req.params.isd });
+
+    }
     
     main()
